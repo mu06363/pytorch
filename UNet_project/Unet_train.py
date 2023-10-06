1002ae8d32a0bdfbe179b8e327ce75c30d10494a
mu06363 <mu06363@yonsei.ac.kr>
2023-10-07 00:31:31 +0900

    data_read first commit

---
+++ UNet_project/Unet_train.py
@@ -0,0 +1,12 @@
+## 라이브러리 추가하기
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
+## 트레이닝 파라미터 설정하기
